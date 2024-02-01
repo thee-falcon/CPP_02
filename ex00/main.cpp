fd08007f0805c8055c94030a823d0adf4bc6043e
Omar Makran <omakran@c1r2p1.1337.ma>
2024-02-01 19:32:19 +0100

    the makefile and int main is done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,19 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 19:25:30 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/01 19:25:31 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/01 19:31:26 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+int main( void ) 
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return (0);
+}
