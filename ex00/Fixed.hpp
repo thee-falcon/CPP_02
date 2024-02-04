ad058b3d2a799ee1dbcf6f9c284ae472aa1e9748
Omar Makran <omakran@c1r2p5.1337.ma>
2024-02-04 18:48:02 +0100

    the header file hpp is done

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,33 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 19:25:27 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/01 19:25:28 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/04 18:46:34 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_H
+# define FIXED_H
+
+# include <iostream>
+
+class FixedPoint
+{
+    int                 _fixedPointNumerValue;
+    static const int    _numberOfFractionalBits = 8;
+
+    public:
+    // constructor
+    FixedPoint();
+    // destructor
+    ~FixedPoint();
+    // copy destructor: hwa wahd special constructor likan ndiro beh wahd object jdid ka copy dyal wahd object f nafs l class.
+    FixedPoint(const FixedPoint& other);
+    // // Copy assignment operator.
+    FixedPoint& operator=(const FixedPoint& other);
+    // returns the raw value of the fixed-point value.
+    int getRawBits( void ) const;
+    // sets the raw value of the fixed-point number.
+    void setRawBits( int const raw );
+};
+
+# endif
