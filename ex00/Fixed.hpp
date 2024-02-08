7088d3f775a741c417a151998d0d4b21945d1a7e
Omar Makran <omakran@c1r1p2.1337.ma>
2024-02-08 21:19:54 +0100

    the ex00 is done alhamdo-lilah

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 19:25:27 by omakran           #+#    #+#             */
-/*   Updated: 2024/02/04 19:04:33 by omakran          ###   ########.fr       */
+/*   Updated: 2024/02/08 21:18:55 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,11 @@ class Fixed
     ~Fixed();
     // copy destructor: hwa wahd special constructor likan ndiro beh wahd object jdid ka copy dyal wahd object f nafs l class.
     Fixed(const Fixed& other);
-    // // Copy assignment operator.
+    // copy assignment operator.
     Fixed& operator=(const Fixed& other);
     // returns the raw value of the fixed-point value.
     int getRawBits( void ) const;
     // sets the raw value of the fixed-point number.
     void setRawBits( int const raw );
 };
-
 # endif
