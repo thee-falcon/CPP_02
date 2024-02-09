8b54c57c0b0500232d608536d5bedf2044e1af8d
thee-falcon <omar.makran.10@gmail.com>
2024-02-09 19:42:39 +0100

    the mainCPP and Makefile is done, I'm working on fixedCPP and headerHPP

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 19:16:58 by omakran           #+#    #+#             */
+/*   Updated: 2024/02/09 19:22:17 by omakran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+    int                 _fixedPointNumerValue;
+    static const int    _numberOfFractionalBits = 8;
+
+    public:
+    // constructors
+    Fixed( void );
+    Fixed (const int num);
+    Fixed (const float num);
+    // destructor
+    ~Fixed();
+    // copy constructor: hwa wahd special constructor likan ndiro beh wahd object jdid ka copy dyal wahd object f nafs l class.
+    Fixed(const Fixed& other);
+    // copy assignment operator.
+    Fixed& operator=(const Fixed& other);
+    // returns the raw value of the fixed-point value.
+    int getRawBits( void ) const;
+    // sets the raw value of the fixed-point number.
+    void setRawBits( int const raw );
+    // converts the fixed-point value to a floating-point value.
+    float toFloat( void ) const;
+    // converts the fixed-point value to an integer value.
+    int toInt( void ) const;
+    //  overload of the insertion («) operator
+    // friend: It allows non-member functions or 
+    // other classes to access private and protected members of a class.
+    friend std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 19:17:05 by omakran           #+#    #+#             */
+/*   Updated: 2024/02/09 19:21:45 by omakran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return (0);
+}
